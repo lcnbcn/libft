00ecc308238d51f40cc5c222218381a5d3419753
Luciano Da Silva Mendes <lucida-s@car14s4.42barcelona.com>
2022-10-04 21:18:21 +0200

    libft v15 (strrchr ai ai)

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,29 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lucida-s <lucida-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/21 16:56:58 by lucida-s          #+#    #+#             */
-/*   Updated: 2022/10/04 21:03:53 by lucida-s         ###   ########.fr       */
+/*   Created: 2022/10/04 20:54:55 by lucida-s          #+#    #+#             */
+/*   Updated: 2022/10/04 21:16:26 by lucida-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_toupper(int c);
-int		ft_isdigit(int c);
-int		ft_tolower(int c);
-int		ft_isalnum(int c);
-size_t	ft_strlen(const char *s);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-#endif
+char	*ft_strrchr(const char *s, int c)
+{
+	char	ch;
+	int		size;
+
+	size = ft_strlen(s);
+
+	ch = (char)c;
+	while (*s <= size && *s != 0)
+		s--;
+	if (*s == ch)
+		return ((char *)s);
+	return (0);
+}
