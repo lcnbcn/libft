14975d0a54cb7f0db72fce4f9fc24f47e7452ef6
Luciano Da Silva Mendes <lucida-s@car14s4.42barcelona.com>
2022-10-06 18:22:18 +0200

    libft v20 (strncmp v03)

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,30 +6,31 @@
 /*   By: lucida-s <lucida-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 09:52:47 by lucida-s          #+#    #+#             */
-/*   Updated: 2022/10/06 10:36:15 by lucida-s         ###   ########.fr       */
+/*   Updated: 2022/10/06 17:55:07 by lucida-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft_h"
+#include "libft.h"
+#include <string.h>
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-
-int	main(void)
-{
-	char	*str_1;
-	char	*str_2;
-	int	n;
-
-	n = 0;
-	str_1 = "Esta string contiene 34 caracteres";
-	str_2 = "Esta string contiene 37 + 1 caracteres";
-	printf("el resultado de la comparacion es: %d"; ft_strncmp(str_1, str_2, n));
-}
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+/*int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int	size_str1;
 	int	size_str2;
 
 	size_str1 = ft_strlen(s1);
+	size_str2 = ft_strlen(s2);
+	return (0);
+}*/
+
+int	main(void)
+{
+	char	*str_1;
+	char	*str_2;
+	size_t	n;
+
+	n = 3;
+	str_1 = "HEllo";
+	str_2 = "hello";
+	printf("el resultado de la comparacion es: %d\n\n", strncmp(str_1, str_2, n));
 }
