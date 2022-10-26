c3fda24387aa14d75ad6ad223ab7daf7e760c547
Luciano Da Silva Mendes <lucida-s@car14s4.42barcelona.com>
2022-10-26 12:14:34 +0200

    libft v30 strlcat modificado ok

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lucida-s <lucida-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 19:03:48 by lucida-s          #+#    #+#             */
-/*   Updated: 2022/10/25 20:10:00 by lucida-s         ###   ########.fr       */
+/*   Updated: 2022/10/26 12:12:23 by lucida-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	dst_len;
-	size_t	src_len;
+	size_t	sl;
+	size_t	dl;
 	size_t	i;
 	size_t	j;
 
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
+	sl = ft_strlen(src);
+	dl = ft_strlen(dst);
 	i = 0;
 	j = 0;
-	if (size > dst_len)
+	if (size > dl)
 	{
-		j = dst_len + src_len;
+		j = sl + dl;
 	}
 	else
 	{
-		j = size + src_len;
+		j = size + sl;
 	}
-	while (src[i] && dst_len + 1 < size)
+	while (src[i] && dl + 1 < size)
 	{
-		dst[dst_len] = src[i];
-		dst_len++;
+		dst[dl] = src[i];
+		dl++;
 		i++;
 	}
-	dst[dst_len] = '\0';
+	dst[dl] = '\0';
 	return (j);
 }
