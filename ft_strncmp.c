77229967113984376a8c333bb4e6dee8d0eecc1d
Luciano Da Silva Mendes <lucida-s@car14s4.42barcelona.com>
2022-10-27 19:00:43 +0200

    libft v32 strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,68 +6,23 @@
 /*   By: lucida-s <lucida-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 12:25:11 by lucida-s          #+#    #+#             */
-/*   Updated: 2022/10/26 16:40:20 by lucida-s         ###   ########.fr       */
+/*   Updated: 2022/10/27 16:53:06 by lucida-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int    ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    size_t i;
-    int cs1;
-    int cs2;
-    i = 0;
-    
-    while (i < n -1)
-    {
-      cs1 = (int)s1[i];
-      cs2 = (int)s2[i];
-      i++;
-    }
-    if (cs1 > cs2)
-      return (1);
-    if (cs1 < cs2)
-      return (-1);
-    else
-      return (0);
-}
-int    ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-    size_t i;
-    int j;
-    int cs1;
-    int cs2;
-    i = 0;
-    
-    while ((cs1 && cs2 != '\n') && i < n)
-    {
-      cs1 = (int)s1[i];
-      cs2 = (int)s2[i];
-      i++;
-    }
-    if (cs1 == cs2)
-      return (0);
-    if (cs1 > cs2)
-      return (1);
-    if (cs1 < cs2)
-      return (-1);
-}
-int    main(void)
-{
-  
-    printf("%d", ft_strncmp("test", "te", 7));
-    return (0);
-}/*
-int	main(void)
-{
-	const char	*str1;	
-	const char	*str2;
-	size_t	nbr;
+	size_t	i;
 
-	str1 = "test";
-	str2 = "testss";
-	nbr = 7;
-	printf("%i", ft_strncmp(str1, str2, nbr));
+	i = -1;
+	while (++i < n)
+	{
+		if (s1[i] != s2[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		else if (s1[i] == '\0' || s2[i] == '\0')
+			return (0);
+	}
 	return (0);
-}*/
+}
