15119424a6c3cbb016a2036a62def0f9ef822ac0
Luciano Da Silva Mendes <lucida-s@cbr7s1.42barcelona.com>
2022-11-08 19:44:07 +0100

    libft v43 putenl ok

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl.c                                       :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lucida-s <lucida-s@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/08 18:31:47 by lucida-s          #+#    #+#             */
-/*   Updated: 2022/11/08 18:58:43 by lucida-s         ###   ########.fr       */
+/*   Created: 2022/11/08 19:21:43 by lucida-s          #+#    #+#             */
+/*   Updated: 2022/11/08 19:40:07 by lucida-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd)
 {
-	int	i;
-
-	i = -1;
-	while (s[++i] != '\0')
-		write(fd, &s[i], 1)
-	
-			[i] = '\n'
-}
-
-
-int	main(void)
-{
-	ft_putendl_fd("hola", 4)
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
